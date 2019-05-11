59cd8adf73117f205908ed4ebe7d9b92bb29c64b
domcross <dkreutz@t-online.de>
2019-05-11 23:49:50 +0200

    CommonPlay basic functionality

--- __init__.py
+++ __init__.py
@@ -17,10 +17,12 @@ import urllib
 from urllib.request import Request
 import re
 
-from adapt.intent import IntentBuilder
-from mycroft.skills.core import MycroftSkill, intent_file_handler
+# from adapt.intent import IntentBuilder
+from mycroft.skills.core import intent_file_handler  # , MycroftSkill
 from mycroft.audio import wait_while_speaking
-from mycroft.util.log import getLogger
+# from mycroft.util.log import getLogger
+from mycroft.skills.common_play_skill import CommonPlaySkill, CPSMatchLevel
+# TODO: use VLC for pause/resume/prev/next
 try:
     from mycroft.skills.audioservice import AudioService
 except:
@@ -29,20 +31,75 @@ except:
 
 __author__ = 'jamespoole'
 
-class PodcastSkill(MycroftSkill):
+
+class PodcastSkill(CommonPlaySkill):
     def __init__(self):
         super(PodcastSkill, self).__init__(name="PodcastSkill")
         self.process = None
         self.audioservice = None
-        self.user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11' 
+        self.user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
 
     def initialize(self):
         if AudioService:
-            self.audioservice = AudioService(self.emitter)
+            self.audioservice = AudioService(self.bus)
+
+    def CPS_match_query_phrase(self, phrase):
+        self.log.debug("phrase {}".format(phrase))
+        # Not ready to play
+        if not self.audioservice:
+            return None
+
+        confidence = 0.0
+        data = None
+
+        if 'podcast' in phrase.lower():
+            bonus = 0.1
+        else:
+            bonus = 0
+
+        podcast_names = [self.settings["nameone"], self.settings["nametwo"], self.settings["namethree"]]
+        podcast_urls = [self.settings["feedone"], self.settings["feedtwo"], self.settings["feedthree"]]
+        # TODO: fuzzy matching
+        data = self.chosen_podcast(phrase, podcast_names, podcast_urls)
+
+        if data:
+            confidence = CPSMatchLevel.EXACT
+
+        self.log.info("phrase: {} confidence: {} data: {}".format(phrase,
+                                                                  confidence,
+                                                                  data))
+        return phrase, confidence, data
+
+    def CPS_start(self, phrase, data):
+            self.log.info("CPS_start phrase: {} data: {}".format(phrase, data))
+
+            parsed_feed = pp.parse(data, urllib.request.urlopen(Request(data,
+                            data=None, headers={'User-Agent': self.user_agent}))
+                          )
+            episode_title = (parsed_feed['episodes'][0]['title'])
+
+            # try and parse the rss feed, some are incompatible
+            try:
+                episode = (parsed_feed["episodes"][0]["enclosures"][0]["url"])
+            except:
+                self.speak_dialog('badrss')
+
+            # check for any redirects
+            episode = urllib.request.urlopen(Request(episode, data=None, headers={'User-Agent': self.user_agent}))
+            redirected_episode = episode.geturl()
+
+            # convert stream to http for mpg123 compatibility
+            http_episode = re.sub('https', 'http', redirected_episode)
+
+            # if audio service module is available use it
+            if self.audioservice:
+                self.audioservice.play(http_episode, episode_title)
+            else:  # othervice use normal mp3 playback
+                self.process = play_mp3(http_episode)
 
     def chosen_podcast(self, utter, podcast_names, podcast_urls):
         for index, name in enumerate(podcast_names):
-            #skip if podcast slot left empty
+            # skip if podcast slot left empty
             if not name:
                 continue
             if name.lower() in utter.lower():
@@ -59,24 +116,24 @@ class PodcastSkill(MycroftSkill):
 
         podcast_names = [self.settings["nameone"], self.settings["nametwo"], self.settings["namethree"]]
         podcast_urls = [self.settings["feedone"], self.settings["feedtwo"], self.settings["feedthree"]]
-        
-        for try_count in range(0,2):
+
+        for try_count in range(0, 2):
             listen_url = self.chosen_podcast(utter, podcast_names, podcast_urls)
             if listen_url:
                 break
             utter = self.get_response('nomatch')
-        else: 
+        else:
             self.speak_dialog('not.found')
-            return False   
+            return False
 
-        #normalise feed and parse it
+        # normalise feed and parse it
         normalised_feed = pp.normalize_feed_url(listen_url)
         parsed_feed = pp.parse(normalised_feed, urllib.request.urlopen(Request(normalised_feed, data=None, headers={'User-Agent': self.user_agent})))
 
-        #Check what episode the user wants
+        # Check what episode the user wants
         episode_index = 0
 
-        #This block adds functionality for the user to choose an episode
+        # This block adds functionality for the user to choose an episode
         while(True):
             episode_title = parsed_feed['episodes'][episode_index]['title']
             podcast_title = parsed_feed['title']
@@ -93,7 +150,7 @@ class PodcastSkill(MycroftSkill):
                     data=data_dict,
                     on_fail='please.repeat')
 
-            #error check
+            # error check
             if response is None:
                 break
 
@@ -105,30 +162,30 @@ class PodcastSkill(MycroftSkill):
             elif "previous" in response:
                 episode_index += 1
             elif "next" in response:
-                #ensure index doesnt go below zero
+                # ensure index doesnt go below zero
                 if episode_index != 0:
                     episode_index -= 1
 
         self.speak("Playing podcast.")
         wait_while_speaking()
 
-        #try and parse the rss feed, some are incompatible
+        # try and parse the rss feed, some are incompatible
         try:
             episode = (parsed_feed["episodes"][episode_index]["enclosures"][0]["url"])
         except:
             self.speak_dialog('badrss')
 
-        #check for any redirects
+        # check for any redirects
         episode = urllib.request.urlopen(Request(episode, data=None, headers={'User-Agent': self.user_agent}))
         redirected_episode = episode.geturl()
 
-        #convert stream to http for mpg123 compatibility
+        # convert stream to http for mpg123 compatibility
         http_episode = re.sub('https', 'http', redirected_episode)
 
         # if audio service module is available use it
         if self.audioservice:
             self.audioservice.play(http_episode, message.data['utterance'])
-        else: # othervice use normal mp3 playback
+        else:  # othervice use normal mp3 playback
             self.process = play_mp3(http_episode)
 
         self.enclosure.mouth_text(episode_title)
@@ -141,33 +198,33 @@ class PodcastSkill(MycroftSkill):
         podcast_names = [self.settings["nameone"], self.settings["nametwo"], self.settings["namethree"]]
         podcast_urls = [self.settings["feedone"], self.settings["feedtwo"], self.settings["feedthree"]]
 
-        #check if the user specified a podcast to check for a new podcast
+        # check if the user specified a podcast to check for a new podcast
         for index, name in enumerate(podcast_names):
-            #skip if podcast slot left empty
+            # skip if podcast slot left empty
             if not name:
                 continue
             if name.lower() in utter.lower():
-                parsed_feed = pp.parse(podcast_urls[index], 
+                parsed_feed = pp.parse(podcast_urls[index],
                                 urllib.request.urlopen(Request(podcast_urls[index], data=None, headers={'User-Agent': self.user_agent})))
                 last_episode = (parsed_feed['episodes'][0]['title'])
 
                 speech_string = "The latest episode of " + name + " is " + last_episode
                 break
         else:
-            #if no podcast names are provided, list all new episodes
+            # if no podcast names are provided, list all new episodes
             new_episodes = []
             for index, url in enumerate(podcast_urls):
-                #skip if url slot left empty
+                # skip if url slot left empty
                 if not url:
                     continue
-                parsed_feed = pp.parse(podcast_urls[index], 
+                parsed_feed = pp.parse(podcast_urls[index],
                                 urllib.request.urlopen(Request(podcast_urls[index], data=None, headers={'User-Agent': self.user_agent})))
                 last_episode = (parsed_feed['episodes'][0]['title'])
                 new_episodes.append(last_episode)
 
-            #skip if i[0] slot left empty
+            # skip if i[0] slot left empty
             elements = [": ".join(i) for i in zip(podcast_names, new_episodes) if i[0]]
-                
+
             speech_string = "The latest episodes are the following: "
             speech_string += ", ".join(elements[:-2] + [" and ".join(elements[-2:])])
 
@@ -180,6 +237,7 @@ class PodcastSkill(MycroftSkill):
             if self.process and self.process.poll() is None:
                 self.process.terminate()
                 self.process.wait()
+
 
 def create_skill():
     return PodcastSkill()
